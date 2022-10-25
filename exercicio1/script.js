17a47887a57e073de879562b9e134af0eabe6a36
Anderson-Schi <jr93@hotmai.com>
2022-10-25 22:15:12 +0100

    Exercicio 1 resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,130 @@
+/* 
+ //a) Declare uma função que imprima Olá, [SEU NOME]!. Chame esta função
+ const imprimaNome = (nome) => {
+    console.log(`Ola ${nome}`);
+    
+ }  
+
+ imprimaNome(`Anderson`);
+
+ //b) Declare uma função que receba um número como parâmetro imprima no console a tabuada do dele. 
+ //Chame esta função várias vezes com números diferentes.
+
+ const tabuada = (num1) => {
+    const tabuNum1 = num1 * 1
+    console.log(`${num1} * 1 = ${tabuNum1}`)
+
+    const tabuNum2 = num1 * 2
+    console.log(`${num1} * 2 = ${tabuNum2}`)
+
+    const tabuNum3 = num1 * 3
+    console.log(`${num1} * 3 = ${tabuNum3}`)
+
+    const tabuNum4 = num1 * 4
+    console.log(`${num1} * 4 = ${tabuNum4}`)
+
+    const tabuNum5 = num1 * 5
+    console.log(`${num1} * 5 = ${tabuNum5}`)
+
+    const tabuNum6 = num1 * 6
+    console.log(`${num1} * 6 = ${tabuNum6}`)
+
+    const tabuNum7 = num1 * 7
+    console.log(`${num1} * 7 = ${tabuNum7}`)
+
+    const tabuNum8 = num1 * 8
+    console.log(`${num1} * 8 = ${tabuNum8}`)
+
+    const tabuNum9 = num1 * 9
+    console.log(`${num1} * 9 = ${tabuNum9}`)
+
+    const tabuNum10 = num1 * 10
+    console.log(`${num1} * 10 = ${tabuNum10}`)
+    
+ }
+ console.log("-------------")
+ tabuada(1);
+ console.log("-------------")
+ tabuada(2);
+ console.log("-------------")
+ tabuada(3);
+ console.log("-------------")
+ tabuada(4);
+ console.log("-------------")
+ tabuada(5);
+ console.log("-------------")
+ tabuada(6);
+ console.log("-------------")
+ tabuada(7);
+ console.log("-------------")
+ tabuada(8);
+ console.log("-------------")
+ tabuada(9);
+ console.log("-------------")
+ tabuada(10);
+ console.log("-------------") */
+
+
+
+
+ const imprimaNome = function (nome) {
+    console.log(`Ola ${nome}`);
+
+ }
+
+ imprimaNome (`Anderson`);
+
+ const tabuada = function (num1){
+    const tabuNum1 = num1 * 1
+    console.log(`${num1} * 1 = ${tabuNum1}`)
+
+    const tabuNum2 = num1 * 2
+    console.log(`${num1} * 2 = ${tabuNum2}`)
+
+    const tabuNum3 = num1 * 3
+    console.log(`${num1} * 3 = ${tabuNum3}`)
+
+    const tabuNum4 = num1 * 4
+    console.log(`${num1} * 4 = ${tabuNum4}`)
+
+    const tabuNum5 = num1 * 5
+    console.log(`${num1} * 5 = ${tabuNum5}`)
+
+    const tabuNum6 = num1 * 6
+    console.log(`${num1} * 6 = ${tabuNum6}`)
+
+    const tabuNum7 = num1 * 7
+    console.log(`${num1} * 7 = ${tabuNum7}`)
+
+    const tabuNum8 = num1 * 8
+    console.log(`${num1} * 8 = ${tabuNum8}`)
+
+    const tabuNum9 = num1 * 9
+    console.log(`${num1} * 9 = ${tabuNum9}`)
+
+    const tabuNum10 = num1 * 10
+    console.log(`${num1} * 10 = ${tabuNum10}`)
+
+ }
+
+ console.log("-------------")
+ tabuada(1);
+ console.log("-------------")
+ tabuada(2);
+ console.log("-------------")
+ tabuada(3);
+ console.log("-------------")
+ tabuada(4);
+ console.log("-------------")
+ tabuada(5);
+ console.log("-------------")
+ tabuada(6);
+ console.log("-------------")
+ tabuada(7);
+ console.log("-------------")
+ tabuada(8);
+ console.log("-------------")
+ tabuada(9);
+ console.log("-------------")
+ tabuada(10);
+ console.log("-------------")
